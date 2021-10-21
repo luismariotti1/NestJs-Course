ae14dc305389c755d36e7225686c51a481774180
luismariotti1 <fernando-mariotti@outlook.com>
2021-10-20 23:24:11 -0300

    first

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.model';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ) {
+    console.log('title', title);
+    console.log('description', description);
+  }
+}
